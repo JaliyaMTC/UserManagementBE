2b4d5e6c3cf44b3861bbb9a1425983bae7cfbed6
chanaka jaliya <cjaliya@legalaid.ab.ca>
2023-02-14 00:03:31 -0700

    initial commit

---
+++ src/test/java/com/example/FirstJavaProject/FirstJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstJavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
